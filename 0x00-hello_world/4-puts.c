a34fcfafedd6570844c50c0907713f18b74b1e94
Chinasaram <anelechinasaram@gmail.com>
2022-03-14 06:26:34 -0700

    task-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * This function prints out "Programming is like building a multilingual puzzle using puts
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multiligual puzzle\n");
+  return(0);
+ }
