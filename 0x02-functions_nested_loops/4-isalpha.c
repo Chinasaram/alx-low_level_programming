fdd3166a589968196c2bcdffc053eaec899577a9
Chinasaram <anelechinasaram@gmail.com>
2022-03-16 15:19:35 -0700

    used ASCII logic for isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a letter is lowercase or uppercase
+ * @c: variable or parameter to be checked
+ * Return: returns 1 if lowercase or lowercase and 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && <= 122)
+
+		return (1);
+
+	else if (c >= 65 && <= 90)
+
+		return (1);
+
+	else
+
+		return (0);
+}
+
