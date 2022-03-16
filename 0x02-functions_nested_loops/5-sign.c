97e47c7cc26b511dcde6eb4eccd5c9f33ce341ba
Chinasaram <anelechinasaram@gmail.com>
2022-03-16 15:38:08 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints signs for numbers or letters > or < 0
+ * @n: the variable to be checked
+ * Return: 1 and + if > 0, 0 and 0 if == 0, -1 and - if < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+
+	else if (n < 0)
+		_putchar('-');
+		return (-1);
+	return (0);
+}
