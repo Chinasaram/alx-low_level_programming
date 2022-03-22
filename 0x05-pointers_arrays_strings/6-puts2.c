8ddbd3bd6eaa8f00388a955ae1b73bb8db11d17c
Chinasaram <anelechinasaram@gmail.com>
2022-03-22 16:15:28 -0700

    strings and arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,17 +8,14 @@
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
+	i = 0;
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
